cfdbd0aadd80dbb455609908f8a364bc17fcefb9
natalia <nataliawdyl@gmail.com>
2024-02-02 21:49:11 -0600

    inicio, primeros int->flex y cup --> ejemplo, aux

--- OLC1_Proyecto1_202200007/src/P1/Inicio.java
+++ OLC1_Proyecto1_202200007/src/P1/Inicio.java
@@ -1,6 +1,8 @@
 
 package P1;
 
+import java.io.StringReader;
+
 /**
  *
  * @author natalia
@@ -10,9 +12,45 @@ public class Inicio {
     public static void main(String[] args) {
         gui inicioM = new gui();
         inicioM.setVisible(true);
+        //funciones.funcion.mostrar("hola");
         
-                
-                
+        //analizadores("src/analizadores/", "Lexer.jflex", "Parser.cup");
+        
+        String entrada = """
+                         MOSTRAR ( 2*7 + 3);
+                         """;
+        //MOSTRAR
+        
+        analizar(entrada); System.out.println(analizadores.Parser.resultado);        
     }
+    
+    public static void analizadores(String ruta, String jflexFile, String cupFile){
+        try {
+            String opcionesJflex[] =  {ruta+jflexFile,"-d",ruta};
+            jflex.Main.generate(opcionesJflex);
+
+            String opcionesCup[] =  {"-destdir", ruta,"-parser","Parser",ruta+cupFile};
+            java_cup.Main.main(opcionesCup);
+            
+        } catch (Exception e) {
+            System.out.println("No se ha podido generar los analizadores");
+            System.out.println(e);
+        }
+    }
+    
+    public static void analizar (String entrada){
+        try {
+            analizadores.Lexer lexer = new analizadores.Lexer(new StringReader(entrada)); 
+            analizadores.Parser parser = new analizadores.Parser(lexer);
+            parser.parse();
+        } catch (Exception e) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println(e);
+        } 
+    }
+    
+
+        
+ 
     
 }
---
+++ OLC1_Proyecto1_202200007/src/funciones/funcion.java
@@ -0,0 +1,13 @@
+
+package funciones;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author natalia
+ */
+public class funcion {
+    public static String mostrar(String expresion){
+        return "System.out.println("+expresion+");";
+    }    
+}
